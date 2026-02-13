46bff004bdf4d7d0b6f362629da2be4748b3e4c3
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-02-13 09:08:19 -0300

    Add files via upload

---
+++ Pessoal - RPG/ficha.js
@@ -0,0 +1,18 @@
+function sistema(){
+    /*
+    guardar os valores de atributos: Agilidade, Força, Intelecto, Presença e Vigor
+    Colocar uma forma de identificar qual classe a pessoa escolheu
+    Definir os valores de vida, sanidade e pontos de esforço baseado na classe que a pessoa escolheu e nos atributos de vigor e presença
+    */
+
+    const agilidade = document.getElementById('agilidade').value;
+    const forca = document.getElementById('forca').value;
+    const intelecto = document.getElementById('intelecto').value;
+    const presenca = document.getElementById('presenca').value;
+    const vigor = document.getElementById('vigor').value;
+
+    const classe = document.getElementById('classe-select');
+    /* Dependendo da classe que o usuário escolher, o cálculo dos stats vai ser diferente */
+};
+
+sistema()
